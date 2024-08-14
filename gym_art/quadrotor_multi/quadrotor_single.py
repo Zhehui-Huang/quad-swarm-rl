d1c89617e05ac93c915a64ac2a6f692fd2f3e75d
Darren Chiu <darrenchiu321@gmail.com>
2024-08-14 15:31:46 -0700

    on floor condition

--- gym_art/quadrotor_multi/quadrotor_single.py
+++ gym_art/quadrotor_multi/quadrotor_single.py
@@ -434,7 +434,7 @@ class QuadrotorSingle:
         done = self.tick > self.ep_len
         
         # End the Episode early if we crash.
-        if (self.dynamics.crashed_wall or self.dynamics.crashed_ceiling or self.dynamics.crashed_floor):
+        if (self.dynamics.crashed_wall or self.dynamics.crashed_ceiling or self.dynamics.crashed_floor or self.dynamics.on_floor):
             done = True
     
         sv = self.state_vector(self)
