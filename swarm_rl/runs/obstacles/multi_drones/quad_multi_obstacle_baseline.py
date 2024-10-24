fb776c8357b1d6031fa8e27458d9a3247f0e1d9d
Zhehui-Huang <zhehuihu@usc.edu>
2024-10-24 15:28:14 -0700

    Minor modifications & Modify setup.py

--- swarm_rl/runs/obstacles/multi_drones/quad_multi_obstacle_baseline.py
+++ swarm_rl/runs/obstacles/multi_drones/quad_multi_obstacle_baseline.py
@@ -1,5 +1,5 @@
 QUAD_BASELINE_CLI_8 = (
-    'python -m swarm_rl.train --env=quadrotor_multi --train_for_env_steps=2000000000 --algo=APPO --use_rnn=False '
+    'python -m swarm_rl.train --env=quadrotor_multi --train_for_env_steps=1000000000 --algo=APPO --use_rnn=False '
     '--num_workers=36 --num_envs_per_worker=4 --learning_rate=0.0001 --ppo_clip_value=5.0 --recurrence=1 '
     '--nonlinearity=tanh --actor_critic_share_weights=False --policy_initialization=xavier_uniform '
     '--adaptive_stddev=False --with_vtrace=False --max_policy_lag=100000000 --rnn_size=16 '
@@ -13,10 +13,11 @@ QUAD_BASELINE_CLI_8 = (
     # Self
     '--quads_obs_repr=xyz_vxyz_R_omega --quads_sim2real=True --quads_encoder_type=attention --quads_num_agents=8 '
     # Neighbor
-    '--quads_neighbor_hidden_size=16 --quads_neighbor_obs_type=pos --quads_collision_hitbox_radius=2.0 '
+    '--quads_neighbor_hidden_size=16 --quads_neighbor_obs_type=pos_vel --quads_collision_hitbox_radius=2.0 '
     '--quads_collision_falloff_radius=4.0 --quads_collision_reward=5.0 --quads_collision_smooth_max_penalty=4.0 '
     '--quads_neighbor_encoder_type=no_encoder --quads_neighbor_visible_num=2 '
     # Obstacles
     '--quads_use_obstacles=True --quads_obst_spawn_area 8 8 --quads_obst_density=0.2 --quads_obst_size=0.3 '
-    '--quads_obst_collision_reward=5.0 --quads_obstacle_obs_type=ToFs'
+    '--quads_obst_collision_reward=5.0 --quads_obstacle_obs_type=ToFs --quads_obst_hidden_size=16 '
+    '--quads_obstacle_tof_resolution=8'
 )
--- swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal.py
+++ swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal.py
@@ -3,25 +3,25 @@ from swarm_rl.runs.obstacles.multi_drones.quad_multi_obstacle_baseline import QU
 
 _params = ParamGrid(
     [
-        ("seed", [6666]),
-        ("quads_obst_collision_prox_weight", [0.001, 0.01, 0.1, 0.5, 1.0]),
-        ("quads_obst_collision_prox_min", [0.01, 0.05, 0.1, 0.2]),
+        ("seed", [0000, 1111, 2222, 3333]),
+        ("exploration_loss_coeff", [0.0, 0.003, 0.01]),
     ]
 )
 
 OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
-    ' --quads_room_dims 8.0 8.0 5.0 --quads_obst_spawn_area 4 4 '
-    '--num_envs_per_worker=4 --rnn_size=16 --quads_obs_repr=xyz_vxyz_R_omega --quads_dynamic_goal=True --normalize_input=True '
-    '--replay_buffer_sample_prob=0.3 --quads_obst_grid_size=0.5 --quads_obst_spawn_center=False --quads_obst_grid_size_range 0.5 0.8 --quads_obst_grid_size_random=True '
-    '--quads_neighbor_visible_num=2 --quads_neighbor_obs_type=pos_vel --quads_neighbor_hidden_size=16 '
-    '--quads_obstacle_tof_resolution=8 --quads_obst_collision_reward=5.0 --quads_obst_collision_prox_max=0.5 '
-    '--quads_obst_hidden_size=16 --quads_obst_density=0.2 --quads_obstacle_obs_type=ToFs --quads_mode=o_random_dynamic_goal '
-    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones '
-    '--wandb_group=md_mo_obst_search'
+    # Obst related
+    ' --quads_room_dims 8.0 8.0 5.0 --quads_obst_spawn_area 4 4 --quads_obst_grid_size=0.5 '
+    '--quads_obst_spawn_center=False --quads_obst_grid_size_range 0.6 1.0 --quads_obst_grid_size_random=True '
+    '--quads_obst_collision_prox_weight=0.01 --quads_obst_collision_prox_min=0.05 --quads_obst_collision_prox_max=0.5 '
+    # Aux
+    '--normalize_input=True --quads_dynamic_goal=True --replay_buffer_sample_prob=0.3 '
+    '--quads_mode=o_random_dynamic_goal '
+    # W & B
+    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones --wandb_group=exploration_loss'
 )
 
 _experiment = Experiment(
-    "md_mo",
+    "exploration_loss",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
