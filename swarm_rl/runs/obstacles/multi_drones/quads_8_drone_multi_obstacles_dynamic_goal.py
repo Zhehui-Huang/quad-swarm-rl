3df942c28329ce1935d9155ad256bfbc0cae4644
Zhehui-Huang <zhehuihu@usc.edu>
2024-10-25 20:34:36 -0700

    Modify runner script

--- swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal.py
+++ swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal.py
@@ -4,24 +4,24 @@ from swarm_rl.runs.obstacles.multi_drones.quad_multi_obstacle_baseline import QU
 _params = ParamGrid(
     [
         ("seed", [0000, 1111, 2222, 3333]),
-        ("exploration_loss_coeff", [0.0, 0.003, 0.01]),
+        ("exploration_loss_coeff", [0.001, 0.003]),
     ]
 )
 
 OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     # Obst related
-    ' --quads_room_dims 8.0 8.0 5.0 --quads_obst_spawn_area 4 4 --quads_obst_grid_size=0.5 '
+    ' --quads_room_dims 8.0 8.0 5.0 --quads_obst_spawn_area 8 4 --quads_obst_grid_size=0.5 '
     '--quads_obst_spawn_center=False --quads_obst_grid_size_range 0.6 1.0 --quads_obst_grid_size_random=True '
     '--quads_obst_collision_prox_weight=0.01 --quads_obst_collision_prox_min=0.05 --quads_obst_collision_prox_max=0.5 '
     # Aux
     '--normalize_input=True --quads_dynamic_goal=True --replay_buffer_sample_prob=0.3 '
     '--quads_mode=o_random_dynamic_goal '
     # W & B
-    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones --wandb_group=exploration_loss'
+    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones --wandb_group=exploration_loss_v2'
 )
 
 _experiment = Experiment(
-    "exploration_loss",
+    "exploration_loss_v2",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
