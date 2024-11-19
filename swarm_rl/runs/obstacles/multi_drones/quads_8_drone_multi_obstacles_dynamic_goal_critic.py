2efe82b8c87cf023a1393be1b0016448481978f9
Zhehui-Huang <zhehuihu@usc.edu>
2024-11-18 16:19:04 -0800

    Search replay

--- swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal_critic.py
+++ swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal_critic.py
@@ -3,7 +3,8 @@ from swarm_rl.runs.obstacles.multi_drones.quad_multi_obstacle_baseline import QU
 
 _params = ParamGrid(
     [
-        ("seed", [0000, 1111, 2222, 3333]),
+        ("seed", [0000, 3333]),
+        ("replay_buffer_sample_prob", [0.5, 0.7, 0.9]),
     ]
 )
 
@@ -14,18 +15,18 @@ OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     '--quads_obst_collision_prox_weight=0.01 --quads_obst_collision_prox_min=0.05 --quads_obst_collision_prox_max=0.5 '
     '--quads_obst_density=0.2 '
     # SBC
-    '--quads_enable_sbc=True --quads_neighbor_range=2.0 --quads_obst_range=2.0 --quads_coeff_effort=0.05 '
-    '--quads_coeff_omega=0.1 --quads_coeff_spin=0.0 --quads_coeff_sbc_acc=0.2 --quads_coeff_sbc_boundary=0.01 '
+    '--quads_enable_sbc=False --quads_neighbor_range=2.0 --quads_obst_range=2.0 --quads_coeff_effort=0.05 '
+    '--quads_coeff_omega=0.2 --quads_coeff_spin=0.0 --quads_coeff_sbc_acc=0.2 --quads_coeff_sbc_boundary=0.01 '
     '--quads_sbc_obst_agg=0.2 --quads_critic_rnn_size=64 '
     # Aux
-    '--normalize_input=True --quads_dynamic_goal=True --replay_buffer_sample_prob=0.3 --exploration_loss_coeff=0.001 '
+    '--normalize_input=True --quads_dynamic_goal=True --exploration_loss_coeff=0.001 '
     '--quads_mode=o_random_dynamic_goal '
     # W & B
-    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones --wandb_group=sbc_pid_v1'
+    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones --wandb_group=replay_v1'
 )
 
 _experiment = Experiment(
-    "sbc_pid_v1",
+    "replay_v1",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
