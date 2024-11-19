5d7985934648a88601aa1e57965a5bd71fbd482b
Zhehui-Huang <zhehuihu@usc.edu>
2024-11-18 18:35:00 -0800

    Modify density random

--- swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal_critic.py
+++ swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal_critic.py
@@ -4,7 +4,7 @@ from swarm_rl.runs.obstacles.multi_drones.quad_multi_obstacle_baseline import QU
 _params = ParamGrid(
     [
         ("seed", [0000, 3333]),
-        ("replay_buffer_sample_prob", [0.5, 0.7, 0.9]),
+        ("quads_obst_density_random", [True]),
     ]
 )
 
@@ -13,7 +13,9 @@ OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     ' --quads_room_dims 8.0 8.0 5.0 --quads_obst_spawn_area 6 4 --quads_obst_grid_size=0.7 '
     '--quads_obst_spawn_center=False --quads_obst_grid_size_range 0.7 1.0 --quads_obst_grid_size_random=True '
     '--quads_obst_collision_prox_weight=0.01 --quads_obst_collision_prox_min=0.05 --quads_obst_collision_prox_max=0.5 '
-    '--quads_obst_density=0.2 '
+    '--quads_obst_density=0.2 --replay_buffer_sample_prob=0.3 '
+    # Random
+    '--quads_obst_density_min=0.2 --quads_obst_density_max=0.8 '
     # SBC
     '--quads_enable_sbc=False --quads_neighbor_range=2.0 --quads_obst_range=2.0 --quads_coeff_effort=0.05 '
     '--quads_coeff_omega=0.2 --quads_coeff_spin=0.0 --quads_coeff_sbc_acc=0.2 --quads_coeff_sbc_boundary=0.01 '
@@ -22,11 +24,11 @@ OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     '--normalize_input=True --quads_dynamic_goal=True --exploration_loss_coeff=0.001 '
     '--quads_mode=o_random_dynamic_goal '
     # W & B
-    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones --wandb_group=replay_v1'
+    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones --wandb_group=random_density_v1'
 )
 
 _experiment = Experiment(
-    "replay_v1",
+    "random_density_v1",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
