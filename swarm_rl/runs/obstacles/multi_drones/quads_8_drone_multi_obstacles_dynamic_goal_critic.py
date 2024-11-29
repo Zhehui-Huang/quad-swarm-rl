0044464271d7eb2b34fb423c5be5cc1b1541c757
Zhehui-Huang <zhehuihu@usc.edu>
2024-11-29 14:15:40 -0800

    Modify runner scripts

--- swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal_critic.py
+++ swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal_critic.py
@@ -3,8 +3,9 @@ from swarm_rl.runs.obstacles.multi_drones.quad_multi_obstacle_baseline import QU
 
 _params = ParamGrid(
     [
-        ("seed", [0000, 1111, 2222, 3333]),
+        ("seed", [0000, 3333, 6666]),
         ("quads_critic_obs", ['octomap', 'ToFs']),
+        ("exploration_loss_coeff", [0.001, 0.003]),
     ]
 )
 
@@ -26,11 +27,11 @@ OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     '--normalize_input=True --quads_dynamic_goal=True --exploration_loss_coeff=0.001 '
     '--quads_mode=o_random_dynamic_goal '
     # W & B
-    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones --wandb_group=scenario_modify_v3'
+    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones --wandb_group=experience_replay_v2'
 )
 
 _experiment = Experiment(
-    "scenario_modify_v3",
+    "experience_replay_v2",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
