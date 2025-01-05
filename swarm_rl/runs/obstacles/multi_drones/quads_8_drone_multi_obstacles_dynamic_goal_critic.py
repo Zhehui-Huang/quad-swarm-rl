7f2efc00474bb66d0f2aab58201580ed7f7e9dd8
Zhehui-Huang <zhehuihu@usc.edu>
2025-01-04 21:36:09 -0800

    Minor modifications

--- swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal_critic.py
+++ swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal_critic.py
@@ -17,15 +17,14 @@ OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     '--quads_obst_density=0.2 --replay_buffer_sample_prob=0.75 --quads_critic_obs=octomap '
     # Random
     '--quads_obst_density_random=False --quads_obst_density_min=0.2 --quads_obst_density_max=0.3 '
-    '--quads_obst_size_random=True --quads_obst_size_min=0.28 --quads_obst_size_max=0.32 '
-    '--quads_obst_noise=0.05 '
+    '--quads_obst_size_random=True --quads_obst_noise=0.05 '
     # SBC
-    '--quads_enable_sbc=False --quads_neighbor_range=2.0 --quads_obst_range=2.0 --quads_coeff_effort=0.05 '
-    '--quads_coeff_omega=0.2 --quads_coeff_spin=0.0 --quads_coeff_sbc_acc=0.2 --quads_coeff_sbc_boundary=0.01 '
+    '--quads_enable_sbc=False --quads_neighbor_range=2.0 --quads_obst_range=2.0 '
+    '--quads_coeff_sbc_acc=0.2 --quads_coeff_sbc_boundary=0.01 '
     '--quads_sbc_obst_agg=0.2 --quads_critic_rnn_size=64 '
     # Aux
-    '--normalize_input=True --quads_dynamic_goal=True --exploration_loss_coeff=0.001 '
-    '--quads_mode=o_random_dynamic_goal '
+    '--normalize_input=True --quads_dynamic_goal=True --exploration_loss_coeff=0.001 --quads_coeff_effort=0.05 '
+    '--quads_coeff_omega=0.2 --quads_coeff_spin=0.0 --quads_mode=o_random_dynamic_goal '
     # Neighbor related
     '--quads_neighbor_obs_update_freq=50 '
     # W & B
