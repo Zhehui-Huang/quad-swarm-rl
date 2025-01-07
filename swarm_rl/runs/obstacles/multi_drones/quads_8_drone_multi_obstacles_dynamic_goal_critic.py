f378c5b30703960a3e66edb6beb4d55fc459864c
Zhehui-Huang <zhehuihu@usc.edu>
2025-01-06 21:45:19 -0800

    modify runner script

--- swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal_critic.py
+++ swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal_critic.py
@@ -20,8 +20,8 @@ OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     '--quads_obst_size_random=True --quads_obst_size_min=0.2 --quads_obst_size_max=0.6 '
     '--quads_obst_noise=0.05 '
     # SBC
-    '--quads_enable_sbc=False --quads_neighbor_range=2.0 --quads_obst_range=2.0 --quads_sbc_obst_agg=0.2 '
-    '--quads_coeff_sbc_acc=0.2 --quads_coeff_sbc_boundary=0.01 '
+    '--quads_enable_sbc=True --quads_neighbor_range=2.0 --quads_obst_range=2.0 --quads_sbc_obst_agg=0.2 '
+    '--quads_coeff_sbc_acc=0.2 --quads_coeff_sbc_boundary=0.0 '
     # Aux
     '--normalize_input=True --quads_dynamic_goal=True --exploration_loss_coeff=0.001 --quads_coeff_effort=0.05 '
     '--quads_coeff_omega=0.2 --quads_coeff_spin=0.0 --quads_mode=o_random_dynamic_goal '
@@ -29,11 +29,11 @@ OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     # Neighbor related
     '--quads_neighbor_obs_update_freq=50 '
     # W & B
-    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones --wandb_group=obst_all_dir_v1'
+    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones --wandb_group=obst_all_dir_modified_sbc_v1'
 )
 
 _experiment = Experiment(
-    "obst_all_dir_v1",
+    "obst_all_dir_modified_sbc_v1",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
