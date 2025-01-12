b00b5bf24e802f1e23573ef73493f9b49196fb76
Zhehui-Huang <zhehuihu@usc.edu>
2025-01-11 21:24:44 -0800

    Change rnn size to 10

--- swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal_critic.py
+++ swarm_rl/runs/obstacles/multi_drones/quads_8_drone_multi_obstacles_dynamic_goal_critic.py
@@ -4,7 +4,6 @@ from swarm_rl.runs.obstacles.multi_drones.quad_multi_obstacle_baseline import QU
 _params = ParamGrid(
     [
         ("seed", [0000, 3333]),
-        ("quads_coeff_effort", [0.1]),
     ]
 )
 
@@ -22,17 +21,19 @@ OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     '--quads_enable_sbc=False --quads_neighbor_range=2.0 --quads_obst_range=2.0 --quads_sbc_obst_agg=0.2 '
     '--quads_coeff_sbc_acc=0.2 --quads_coeff_sbc_boundary=0.0 '
     # Aux
-    '--normalize_input=True --quads_dynamic_goal=True --exploration_loss_coeff=0.001 '
+    '--normalize_input=True --quads_dynamic_goal=True --exploration_loss_coeff=0.001 --quads_coeff_effort=0.05 '
     '--quads_coeff_omega=0.2 --quads_coeff_spin=0.0 --quads_mode=o_random_dynamic_goal '
     '--quads_critic_rnn_size=64 --quads_critic_obs=octomap --replay_buffer_sample_prob=0.75 '
     # Neighbor related
     '--quads_neighbor_obs_update_freq=50 '
+    # Model
+    '--rnn_size=10 --quads_neighbor_hidden_size=10 --quads_obst_hidden_size=10 '
     # W & B
-    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones --wandb_group=adir_ro_search_rew_effort_v1'
+    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones --wandb_group=adir_ro_search_rnn_size_v1'
 )
 
 _experiment = Experiment(
-    "adir_ro_search_rew_effort_v1",
+    "adir_ro_search_rnn_size_v1",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
